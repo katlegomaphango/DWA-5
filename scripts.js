4013d177fd98fa7ff06b774aadb0ce6098fa8cf8
Katlego Maphango <mphangokatlego08@gmail.com>
2023-05-26 10:42:40 +0200

    static typing

--- scripts.js
+++ scripts.js
@@ -1,5 +1,9 @@
+// @ts-check
+
 const form = document.querySelector("[data-form]");
 const result = document.querySelector("[data-result]");
+
+if (form === null) throw new console.error('form undefined');
 
 form.addEventListener("submit", (event) => {
     event.preventDefault();
@@ -8,6 +12,9 @@ form.addEventListener("submit", (event) => {
     
     if (dividend === "" || divider === "") {
         result.innerText = 'Division not performed. Both values are required in inputs. Try again'
+    } else if (divider <= 0) {
+        result.innerText = 'Division not performed. Invalid number provided. Try again'
+        console.trace()
     } else {
         result.innerText = Math.floor(dividend / divider);
     }
