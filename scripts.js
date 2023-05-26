b7abffd20ace3ec8e0b40b90954849a893052eba
Katlego Maphango <mphangokatlego08@gmail.com>
2023-05-26 10:57:29 +0200

    fixed errors

--- scripts.js
+++ scripts.js
@@ -3,19 +3,27 @@
 const form = document.querySelector("[data-form]");
 const result = document.querySelector("[data-result]");
 
-if (form === null) throw new console.error('form undefined');
+if (form === null || result === null) throw new console.error('form and result are undefined');
 
 form.addEventListener("submit", (event) => {
     event.preventDefault();
+
+    //@ts-ignore
     const entries = new FormData(event.target);
     const { dividend, divider } = Object.fromEntries(entries);
     
     if (dividend === "" || divider === "") {
-        result.innerText = 'Division not performed. Both values are required in inputs. Try again'
-    } else if (divider <= 0) {
-        result.innerText = 'Division not performed. Invalid number provided. Try again'
+        result.textContent = 'Division not performed. Both values are required in inputs. Try again'
+        return
+    }
+
+    const dividendNum = parseInt(dividend.toString())
+    const dividerNum = parseInt(divider.toString())
+    
+    if (dividerNum <= 0) {
+        result.textContent = 'Division not performed. Invalid number provided. Try again'
         console.trace()
     } else {
-        result.innerText = Math.floor(dividend / divider);
+        result.textContent = Math.floor(dividendNum / dividerNum).toString();
     }
 });
